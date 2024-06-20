daf16356da06d2bedb23f8e04974c92b5d1508c2
flamedfury <heavy.job8629@fastmail.com>
2024-06-20 21:59:43 +1200

    you there?

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy ("src/assets/css/styles.css");
+  eleventyConfig.addPassthroughCopy ("src/assets/");
 
   return {
     dir: {
